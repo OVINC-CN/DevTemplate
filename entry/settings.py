624982c668d27298cb256c8a8fb0ac6d08e8eb13
Oren Zhang <orenzhang@ovinc.cn>
2022-12-13 14:30:10 +0800

    feature: ssl support for dev (#5)

--- entry/settings.py
+++ entry/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "sslserver",
     "apps.account",
     "apps.cel",
     "apps.home",
